2e2361439de8614e8a48b62add0372b1d47577ab
Paul Valerie GOMA <paulvalerie.goma@edu.ece.fr>
2024-05-06 04:44:04 +0200

    deploy my website

--- react_release/src/components/Infocard/infocard.jsx
+++ react_release/src/components/Infocard/infocard.jsx
@@ -3,6 +3,7 @@ import MailButton from './MailButton';
 import './infocard.css';
 import { GoMail } from "react-icons/go";
 import { InfocardIcons } from '../../utils/Icons';
+import profil from '../../assets/Profil_photo.jpg'
 
 export default function Infocard() {
     
@@ -10,7 +11,7 @@ export default function Infocard() {
         <h1 className='name'>Paul Valerie GOMA</h1>
         <h2 className='student'>Etudiant, France</h2>
         <div >
-            <img src="C:\Users\paulg\Documents\Web_Project\Mon_Portfolio\react_release\dist\Profil_photo.jpg" alt = "Profile picture of Paul Valerie GOMA" width="250" height="auto" className='picture'/> 
+            <img src={profil} alt = "Profile picture of Paul Valerie GOMA" width="250" height="auto" className='picture'/> 
         </div>
         <h3 className='portfolio'>mon portfolio</h3>
         <section className='icon-container'>
